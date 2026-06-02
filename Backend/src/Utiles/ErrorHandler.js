7c391b413df032e92d6470c2e769aea9948890b8
anurag0577 <anurag0577@gmail.com>
2026-06-02 23:43:20 +0530

    Write error handler for handling error properly

---
+++ Backend/src/Utiles/ErrorHandler.js
@@ -0,0 +1,23 @@
+class apiError extends Error{
+    constructor(
+        statusCode,
+        message = 'Something went wrong!',
+        errors = [],
+        stack = ''
+    ){
+
+        super(message);
+
+        this.statusCode = statusCode;
+        this.data = null;
+        this.success = false
+
+        if (stack) {
+            this.stack = stack;
+        } else {
+            Error.captureStackTrace(this, this.constructor); // we are telling node, "Hey, look at the exact file and line where this error was thrown right now, record the whole history of how we got here, and save that map inside this.stack."
+        }
+    }
+}
+
+export {apiError}
