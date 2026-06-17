ed5ba6b824c71117da8e56fd3f10a24aa2c00bef
anurag0577 <anurag0577@gmail.com>
2026-06-17 18:03:20 +0530

    create user signup functionality

--- Frontend/src/pages/Signup.jsx
+++ Frontend/src/pages/Signup.jsx
@@ -1,44 +1,47 @@
-import { useEffect, useState } from "react"
+import {  useState } from "react"
+import { useNavigate } from "react-router";
 import logo_img from '../assets/Images/the_democratic_club_logo_white.png'; 
+import { useMutation } from "@tanstack/react-query";
+import useAuthStore from "../store/useAuthStore";
+import api from "../api/axios";
 export function Signup(){
 
-    const [data, setData] = useState(null)
+    const navigate = useNavigate();
+    const {updateCurrentUserInfo} = useAuthStore();
     const [firstname, setFirstname] = useState('');
     const [lastname, setLastname] = useState('')
     const [email, setEmail] = useState('')
     const [password, setPassword] = useState('')
 
-  useEffect(() => {
-    const response = fetch('http://localhost:3000/', {
-      method: 'GET',
-      credentials: 'include'
-    })
-    response.then((res) => res.json())
-    .then(setData)
-  }, [])
+    const signupUser = useMutation({
+      mutationKey: ['userSignup'],
+      mutationFn: async ({firstname, lastname, email, password}) => {
+        const response = await api.post('/user/signup', {
+          firstname, 
+          lastname,
+          email,
+          password
+        },{
+          withCredentials: true
+        })
 
-  const handleSignup = async() => {
-    fetch('http://localhost:3000/user/signup', {
-      method: 'POST',
-      credentials: 'include',
-      headers: {
-        'Content-Type': 'application/json'
+        return response.data.data;
       },
-      body: JSON.stringify({firstname, lastname, email, password}),
+      onSuccess: (data) => {
+        updateCurrentUserInfo(data.accessToken)
+        navigate('/')
+      },
+      onError: (error) => console.error('Error registering user:', error)
     })
-    .then(response => response.json())
-    .then(data => console.log(data))
-  }
-  
-  console.log('Data fetched successfully:', data)
+
     return (
         <>
             <div className="page-container h-screen flex flex-col lg:flex-row bg-black text-white ">
               <div className="left-container w-full  lg:w-[50%] flex items-center justify-center">
-                <img src={logo_img} alt="The Democratic Club logo" className="w-auto max-h-[80px] lg:w-full lg:max-h-fit" />
+                <img src={logo_img} alt="The Democratic Club logo" className="w-auto max-h-20 lg:w-full lg:max-h-fit" />
               </div>
               <div className="right-container w-full flex-1 lg:w-[50%] flex justify-center items-center ">
-                <div className="signup-form-container  max-w-[340px] w-[80%] flex flex-col justify-center gap-y-10 border-1 border-[#1f1f1f] rounded-xl p-5 h-[80%] lg:h-[80%] lg:max-h-[520px] ">
+                <div className="signup-form-container  max-w-85 w-[80%] flex flex-col justify-center gap-y-10 border border-[#1f1f1f] rounded-xl p-5 h-[80%] lg:h-[80%] lg:max-h-[520px] ">
                     <div className="form-header flex flex-col justify-center items-center">
                       <h3 className="form-heading text-2xl font-bold text-center tracking-tighter">Create new account</h3>
                       <p className="form-description text-center text-sm">We just need a few details to get you started.</p>
@@ -103,8 +106,11 @@ export function Signup(){
                         </div>
                       </div>
                     </div>
-                    <div className="form-button">
-                        <button className="button form-submit text-black bg-white w-full py-1.5 rounded" onClick={handleSignup}>Create</button>
+                    <div className="form-button flex flex-col justify-center items-center">
+                        <button className="button form-submit text-black bg-white w-full py-1.5 rounded" onClick={() => signupUser.mutate({firstname, lastname, email, password})}>Create</button>
+                        <p className="text-[12px] text-[#b6b6b6] mt-1" >Already have account? 
+                          <a href="/login" className="pl-1 italic underline">Login</a>
+                        </p>
                     </div>
                 </div>
               </div>
