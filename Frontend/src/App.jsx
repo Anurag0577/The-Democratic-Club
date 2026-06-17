9f1db9f8abccdc07137eda06a718230d80e285c6
anurag0577 <anurag0577@gmail.com>
2026-06-17 17:57:26 +0530

    Setup router and queryClient for apis

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,13 +1,57 @@
 // import { Signup } from './pages/Signup.jsx'
 import { Login } from './pages/Login.jsx'
+import { createBrowserRouter, Outlet, RouterProvider } from 'react-router';
+import { Signup } from './pages/Signup.jsx';
+import NotFoundPage from './components/NotFound.jsx';
+import { QueryClientProvider, QueryClient } from '@tanstack/react-query'
+import { Homepage } from './components/Homepage.jsx';
+import { AuthModel } from './components/AuthModel.jsx';
 
+// ✅ Move this OUTSIDE the App component
+const client = new QueryClient();
+
+function RootLayout(){
+
+  return(
+    <>
+      <AuthModel/>
+      <Outlet/>
+    </>
+  )
+}
 
 function App() {
 
+  const router = createBrowserRouter([
+    {
+      path: '/',
+      element: <RootLayout/>,
+      children: [
+        {
+          path: '/login',
+          element: <Login/>
+        },
+        {
+          path: '/signup',
+          element: <Signup/>
+        },
+        {
+          path: '/',
+          element: <Homepage/>
+        },
+        {
+          path: '*',
+          element: <NotFoundPage/>
+        }
+      ]
+    }
+  ])
+
   return (
     <>
-      {/* <Signup /> */}
-      <Login />
+      <QueryClientProvider client={client} >
+        <RouterProvider router={router} />
+      </QueryClientProvider>
     </>
   )
 }
