755c991d3f2b0132fdb3e3f1df1c64a490ebf66a
anurag0577 <anurag0577@gmail.com>
2026-06-18 06:12:50 +0530

    create user login functionality

--- Frontend/src/pages/Login.jsx
+++ Frontend/src/pages/Login.jsx
@@ -1,45 +1,50 @@
-import { useEffect, useState } from "react"
+import { useState } from "react"
 import logo_img from '../assets/Images/the_democratic_club_logo_white.png'; 
+import { useMutation } from "@tanstack/react-query";
+import api from "../api/axios";
+import useAuthStore from "../store/useAuthStore";
+import { useNavigate } from "react-router";
+
 export function Login(){
 
-    const [data, setData] = useState(null)
-    const [email, setEmail] = useState('')
-    const [password, setPassword] = useState('')
-
-  useEffect(() => {
-    const response = fetch('http://localhost:3000/', {
-      method: 'GET',
-      credentials: 'include'
-    })
-    response.then((res) => res.json())
-    .then(setData)
-  }, [])
-
-const handleLogin = () => {
-  fetch('http://localhost:3000/user/login', {
-    method: 'POST',
-    credentials: 'include',
-    headers: {
-      'Content-Type': 'application/json'
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  const navigate = useNavigate();
+  const {updateCurrentUserInfo} = useAuthStore();
+  const loginUser = useMutation({
+    mutationKey: ['loginUser'],
+    mutationFn: async({email, password}) => {
+      const response = await api.post('/user/login', {
+        email,
+        password
+      },
+      {
+        withCredentials: true
+      }
+    )
+      return response.data.data;
     },
-    body: JSON.stringify({ email, password })
+    onSuccess: (data) => {
+      updateCurrentUserInfo(data.accessToken);
+      navigate('/');
+    },
+    onError: (error) => {
+      console.log('Error during login', error)
+    }
   })
-  .then(res => res.json())
-  .then(data => console.log(data))
-}
   
-  console.log('Data fetched successfully:', data)
+
     return (
         <>
             <div className="page-container h-screen flex flex-col lg:flex-row bg-black text-white ">
               <div className="left-container w-full  lg:w-[50%] flex items-center justify-center">
-                <img src={logo_img} alt="The Democratic Club logo" className="w-auto max-h-[80px] lg:w-full lg:max-h-fit" />
+                <img src={logo_img} alt="The Democratic Club logo" className="w-auto max-h-20 lg:w-full lg:max-h-fit" />
               </div>
               <div className="right-container w-full flex-1 lg:w-[50%] flex justify-center items-center ">
-                <div className="signup-form-container border-dashed  max-w-[340px] w-[80%] flex flex-col justify-center gap-y-10 border-1 border-gray-400 rounded-3xl p-5 h-[70%] lg:h-[80%] lg:max-h-[520px] ">
+                <div className="signup-form-container  max-w-85 w-[80%] flex flex-col justify-center gap-y-10 border border-[#1f1f1f] rounded-xl p-5 h-[70%] lg:h-[80%] lg:max-h-[520px] ">
                     <div className="form-header flex flex-col justify-center items-center">
                       <h3 className="form-heading text-2xl font-bold text-center tracking-tighter">Welcome back!</h3>
-                      <p className="form-description text-center text-sm">Enter your credentials to login to your account.</p>
+                      <p className="form-description text-center text-sm text-[#b6b6b6]">Enter your credentials to login to your account.</p>
                     </div>
                     <div className="form-body flex flex-col gap-y-3">
                       <div className="email">
@@ -52,7 +57,7 @@ const handleLogin = () => {
                               e.preventDefault();
                               setEmail(e.target.value)
                             }}
-                            className="email my-2 border border-gray-400 rounded-2xl px-3 h-9"
+                            className="email my-2 border border-[#1f1f1f] rounded px-3 h-9"
                             required 
                           ></input>
                         </div>
@@ -67,14 +72,17 @@ const handleLogin = () => {
                               e.preventDefault();
                               setPassword(e.target.value)
                             }}
-                            className="password my-2 border border-gray-400 rounded-2xl px-3 h-9"
+                            className="password my-2 border border-[#1f1f1f] rounded px-3 h-9"
                             required 
                           ></input>
                         </div>
                       </div>
                     </div>
-                    <div className="form-button">
-                        <button className="button form-submit text-black bg-white w-full py-1.5 rounded-2xl" onClick={handleLogin} >Create</button>
+                    <div className="form-button flex flex-col justify-center items-center">
+                        <button className="button form-submit text-black bg-white w-full py-1.5 rounded" onClick={() => loginUser.mutate({email, password})} >Login</button>
+                        <p className="text-[12px] text-[#b6b6b6] mt-1" >Don't have account? 
+                          <a href="/signup" className="pl-1 italic underline">Create account.</a>
+                        </p>
                     </div>
                 </div>
               </div>
