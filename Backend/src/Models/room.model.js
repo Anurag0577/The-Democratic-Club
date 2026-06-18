b6022dff2349edd4fa7ba3f6e7e0cd0beceb3a60
anurag0577 <anurag0577@gmail.com>
2026-06-18 06:15:30 +0530

    code functions to add or remove members in the room

--- Backend/src/Models/room.model.js
+++ Backend/src/Models/room.model.js
@@ -55,3 +55,70 @@ const roomSchema = mongoose.Schema({
 {
     timestamps: true 
 })
+
+// generate roomCode (if not already exist) before saving
+roomSchema.pre('save', async function() {
+
+    if(this.roomCode) return // check the room already have a roomCode or not
+
+    // function to generate random string
+    const generateRandomString  = (length) => {
+        const characters  = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'
+        const result = ''
+
+        for(i=0; i<=length; i++){
+            const randomIndex = Math.floor(Math.random() * characters.length)
+            result = result + characters.charAt(randomIndex)
+        }
+
+        return result;
+    }
+
+    const isUnique = false;
+    const generatedRoomCode = '';
+
+    while(!isUnique){
+        generatedRoomCode = generateRandomString(6); // generate a 6 digit random string
+
+        // check whether this string is unique or not
+        const roomExist = await Room.findOne({roomCode: generatedRoomCode})
+
+        // is same code room not exist, make isUnique true.
+        if(!roomExist){
+            isUnique = true;
+        }
+    } 
+    this.roomCode = generatedRoomCode;
+})
+
+
+// add new members in the room
+
+roomSchema.methods.addNewMember = async function(userId){
+    // check user is already in a room or not
+    const isAlreadyInRoom = this.members.some(memberId => memberId.toString() === userId.toString());
+
+    if(isAlreadyInRoom){
+        return {
+            success: false,
+            message: `user ${userId} already is a member of the room.`
+        }
+    }
+
+    this.member.push(userId)
+    await this.save();
+
+    return {success: true, message: `User ${userId} is now the member of the room.`}
+}
+
+roomSchema.methods.removeMember = async function(userId){
+    // remove the user from the room
+    this.members.pull(userId)
+    await this.save();
+
+    return {success: true, message: `User ${userId} is successfull removed from the room.`}
+}
+
+const Room  = mongoose.model('Room', roomSchema);
+
+export {Room}
