f69d1d9fe2984ba9bf35ab0fa48bee91ee5e1cac
anurag0577 <anurag0577@gmail.com>
2026-06-29 23:51:21 +0530

    Fix apiError improper response issue

--- Backend/src/Utiles/ErrorHandler.js
+++ Backend/src/Utiles/ErrorHandler.js
@@ -1,23 +1,28 @@
-class ApiError extends Error{
+class ApiError extends Error {
     constructor(
         statusCode,
         message = 'Something went wrong!',
         errors = [],
         stack = ''
-    ){
-
+    ) {
         super(message);
 
         this.statusCode = statusCode;
-        this.data = null;
-        this.success = false
+        this.success = false;
+        Object.defineProperty(this, 'message', {
+            value: message,
+            enumerable: true, 
+            writable: true,
+            configurable: true
+        });
+        this.errors = errors;
 
         if (stack) {
             this.stack = stack;
         } else {
-            Error.captureStackTrace(this, this.constructor); // we are telling node, "Hey, look at the exact file and line where this error was thrown right now, record the whole history of how we got here, and save that map inside this.stack."
+            Error.captureStackTrace(this, this.constructor);
         }
     }
 }
 
-export {ApiError}
+export { ApiError }
